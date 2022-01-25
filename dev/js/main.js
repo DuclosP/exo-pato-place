223b517f89be8f864b0c491a7c1b324207381a0c
duclos pierre <duclospi@gmail.com>
2022-01-25 14:38:13 +0100

    3 pages terminée, js fonctionnel

--- dev/js/main.js
+++ dev/js/main.js
@@ -7,9 +7,25 @@ btn.forEach(element => {
         aside.classList.toggle("apparition")
     } )
 });
+//le sticky
+let nav = document.querySelector(".nav-bg");
+let ancre = document.querySelector(".anchor-menu");
+let logo = document.getElementById('logo');
 
-
-
-
-
-
+//récuperé la distance de la nav
+let navTop = nav.offsetTop;
+//ajout de l'évent sur le scrolling de windows
+window.addEventListener('scroll', () => {
+    //comparé la position du scroll avec la navtop
+    if (window.scrollY > navTop) {
+        //ajouter la classe sticky
+        nav.classList.add("sticky");
+        ancre.classList.add("sticky");
+        logo.setAttribute("src", "/img/logo2.png")
+    } else {
+        //remove la classe sticky
+        nav.classList.remove("sticky");
+        ancre.classList.remove("sticky")
+        logo.setAttribute("src", "/img/logo.png")
+    }
+})
